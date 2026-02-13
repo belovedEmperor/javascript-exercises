d5d776359cfa1a69d645e6d7f0d51ce01f3f44eb
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-02-13 18:41:33 -0500

    Completed foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const currentAge =
+      (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+    const oldestAge =
+      (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
 
+    return currentAge > oldestAge ? person : oldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
