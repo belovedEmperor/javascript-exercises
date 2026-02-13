e88a93f0cb1645cfce63988c019120716f996621
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-02-13 17:56:11 -0500

    Completed foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  number = Number(number);
+  if (number < 0) return "OOPS";
+  if (isNaN(Number(number))) return "OOPS";
+  if (number === 0) return 0;
+  if (number === 1 || number === 2) return 1;
+  let numbers = [1, 1];
 
+  for (let i = 1; i < number; i++) {
+    numbers.push(numbers[i - 1] + numbers[i]);
+    console.log(numbers);
+  }
+  return numbers[number - 1];
 };
 
 // Do not edit below this line
