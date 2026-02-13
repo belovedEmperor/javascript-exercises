4450922bb1d5b7bdf4aba137978f1a5afb56b5a5
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-02-13 17:28:49 -0500

    Completed foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let palindrome = str.replaceAll(" ", "");
+  palindrome = palindrome.replaceAll(".", "");
+  palindrome = palindrome.replaceAll("''", "");
+  palindrome = palindrome.replaceAll('"', "");
+  palindrome = palindrome.replaceAll(",", "");
+  palindrome = palindrome.replaceAll("!", "");
+  palindrome = palindrome.toLowerCase();
 
+  let splitPalindrome = [];
+  if (palindrome.length % 2 === 1) {
+    splitPalindrome.push(palindrome.slice(0, palindrome.length / 2));
+    splitPalindrome.push(
+      palindrome.slice(palindrome.length / 2 + 1, palindrome.length),
+    );
+  } else {
+    splitPalindrome.push(palindrome.slice(0, palindrome.length / 2));
+    splitPalindrome.push(
+      palindrome.slice(palindrome.length / 2, palindrome.length),
+    );
+  }
+  console.log(
+    splitPalindrome[0].split("").reverse().join("") === splitPalindrome[1],
+  );
+
+  if (splitPalindrome[0].split("").reverse().join("") === splitPalindrome[1])
+    return true;
+  return false;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
