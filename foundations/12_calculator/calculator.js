b051cfc35901fdf8aa15b46ec51585daee21252a
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-02-13 17:08:06 -0500

    Completed foundations/12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  if (numbers.length === 0) return 0;
+  return numbers.reduce((sum, element) => sum + element);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  if (numbers.length === 0) return 0;
+  return numbers.reduce((sum, element) => sum * element);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) return 1;
+  let total = number;
+  for (let currentNumber = number - 1; currentNumber > 0; currentNumber--) {
+    total *= currentNumber;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
