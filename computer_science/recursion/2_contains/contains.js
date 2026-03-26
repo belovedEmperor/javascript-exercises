0a07670f2e1288f3921ac89121c15153d31eb881
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-03-26 11:57:10 -0400

    Complete computer_science/recursion/2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,14 @@
-const contains = function() {
-  
+const contains = function (input, expected) {
+  if (input === expected) return true;
+  else if (Number.isNaN(expected) && Number.isNaN(input)) return true;
+
+  if (typeof input === "object" && input !== null) {
+    for (const key in input) {
+      if (contains(input[key], expected)) return true;
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
