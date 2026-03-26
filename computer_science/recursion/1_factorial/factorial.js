fa23739b49559d1f7f938ccb96eda029ae67e721
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-03-26 11:24:54 -0400

    Complete computer_science/recursion/1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,11 @@
-const factorial = function() {
+const factorial = function (number) {
+  if (typeof number !== "number" || number < 0 || String(number).includes("."))
+    return undefined;
+  if (number === 0) return 1;
 
+  return factorial(number - 1) * number;
 };
 
 // Do not edit below this line
 module.exports = factorial;
+
